620be850fc7d9310657c1f6753e53990e5b1978c
orth <s6vaorth@uni-bonn.de>
2021-06-21 11:05:14 +0200

    figuren klassen

--- Chess/src/board.java
+++ Chess/src/board.java
@@ -1,11 +1,13 @@
 
-public class board 
+public class board
 {
 
 	public static void main(String[] args)
 	{
 	
-		int [] [] board = new int [8] [8];
+		int x = 8;
+		int y = 8;
+		int [] [] board = new int [x] [y];
 
 		
 	}
