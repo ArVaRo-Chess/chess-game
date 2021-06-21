6ca3adccdd29c0357c312db1f8fb3d7dd86729d4
totallynotadolphin <totallynotadolphin@riseup.net>
2021-06-21 13:38:00 +0200

    game skeleton code

--- Chess/src/game.java
+++ Chess/src/game.java
@@ -1,4 +1,26 @@
+public class Game {
 
-public class game {
+	public static void main(String args[])
+	{
+		load();
+		while (true) {
+			update();
+			draw();
+	}
+
+	private static void load()
+	{
+
+	}
+
+	private static void update()
+	{
+
+	}
+
+	private static void draw()
+	{
+
+	}
 
 }
