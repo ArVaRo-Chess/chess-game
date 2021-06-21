1b36ba6a80a801d2c75fe66056643f8761b06360
totallynotadolphin <totallynotadolphin@riseup.net>
2021-06-21 14:13:43 +0200

    progress movement code for king

--- Chess/src/king.java
+++ Chess/src/king.java
@@ -1,10 +1,31 @@
 
-public class king { //
+public class king {
 
 	public int [] [] getMovementOptions()
 	{
-		int spaces [] [] = new int [8] [8];
-		
+		int position[] = {4,4};
+
+		int spaces[][] = new int [8] [8];
+
+		int all_spaces[][] = getAllPossibleSpaces(int position[]);
+
+		return spaces;
 	}
-	
+
+	private int [] [] getAllPossibleSpaces(int position[])
+	{
+		int x = position[0];
+		int y = position[1];
+
+		int all_spaces[8][2] = {{x-1, y-1},
+		                        {x, y-1},
+		                        {x+1, y-1},
+		                        {x-1, y},
+		                        {x+1, y},
+		                        {x-1, y+1},
+		                        {x, y+1},
+		                        {x-1, y+1}};
+		return all_spaces;
+	}
+
 }
